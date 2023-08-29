99f53e5fb3190174201f4fede95ae3dd0ee2601a
Abdel <113614023+TAHIRI1985@users.noreply.github.com>
2023-08-30 00:57:37 +0200

    Application react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Apropos from "./pages/Apropos";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+
+      <Route path="/" element={<Home />} />
+      <Route path="/apropos" element={<Apropos />} />
+      {/* si ya un erreur dans url rederiction vers l'acceuil */}
+      <Route path="/*" element={<Home />} />
+     
+      
+      
+    
+    </Routes>
+    
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Apropos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Apropos = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+           <p></p>
+        </div>
+    );
+};
+
+export default Apropos;
